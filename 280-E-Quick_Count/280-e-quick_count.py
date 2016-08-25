81f1a0a5ab5c24323a274560057d466a0b12ed3d
Malcolm Walters <MalWalters@users.noreply.github.com>
2016-08-25 17:01:38 +1000

    Separate functions for each hand
    Separated main function in to two separate ones. Seems to be more sensible and it works. Invalid is controlled by try -> except on printing the functions.

--- 280-E-Quick_Count/280-e-quick_count.py
+++ 280-E-Quick_Count/280-e-quick_count.py
@@ -1,34 +1,30 @@
 input = open('280-input.txt').read().split()
 
-def countfingers(fingerpos):
-    
-    left = fingerpos[:5]
-    right = fingerpos[5:]
-    valid = True
-    number = 0
+number = 0
 
-# Count right
+def countright(fingers, number):
     for i in range(4,1,-1):
-        if (right[i] == "0" and number > i):
-            valid = False
-        elif (right[i] == "1" and number < i):
+        if (fingers[i] == "0" and number > i):
+            return "Invalid"
+        elif (fingers[i] == "1" and number < i):
             number += i
-    if right[0] == "1":
+    if fingers[0] == "1":
         number +=5
+    return number
 
-# Count left
+def countleft(fingers, number):
     for i in range(1,4):
-        if (left[i] == "0" and number >= 40 - i * 10):
-            valid = False
-        elif (left[i] == "1" and number <= 40 - i * 10):
-            number += i * 10   
-    if left[4] == "1":
+        if (fingers[i] == "0" and number >= 40 - i * 10):
+            return "Invalid"
+        elif (fingers[i] == "1" and number <= 40 - i * 10):
+            number += i * 10
+    if fingers[4] == "1":
         number += 50
-    
-    if valid:
-        return str(number)
-    else:
-        return "Invalid"
+    return(number)
 
 for lines in input:
-    print(lines + " -> " + countfingers(lines))
+    try:
+        print(lines + " -> " + str(countleft(lines[:5], number) + countright(lines[5:],number)))
+    except:
+        print(lines + " -> Invalid")
+
